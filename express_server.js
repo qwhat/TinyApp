dc23d54f591267bc03b744a23905fc8c9368b36c
qwhat <justinmao@live.ca>
2019-01-16 20:54:44 +0000

    day 2 work for tinyApp project

---
+++ express_server.js
@@ -0,0 +1,66 @@
+var express = require("express");
+var app = express();
+var PORT = 3001;
+const uuidv4 = require('uuid/v4');
+
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
+  "cd11e9": "http://www.youtube.ca"
+};
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+  res.send("IT\'S ALIVE!!!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
+
+  res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  let longURL = req.body.longURL
+  let shortURL = generateRandomString()
+  urlDatabase[shortURL] = longURL;
+  res.redirect('/urls/' + shortURL);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+
+function generateRandomString() {
+  return uuidv4().substr(0, 6)
+}
